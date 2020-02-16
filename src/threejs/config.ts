c7b027c62d12368fbd9621c6339c2b01d029e172
Haoran Zhang <haoranzhang@icloud.com>
2020-02-16 17:11:02 +0000

    Add color to config
    Add color to config

--- src/threejs/config.ts
+++ src/threejs/config.ts
@@ -2,5 +2,6 @@ export const colorPalette = {
   WHITE: 0xffffff,
   LIGHT_CREAM: 0xfffff7,
   LIGHT_YELLOW: 0xffccaa,
-  PANTONE_BLUE_2020: 0x0f4c81
+  PANTONE_BLUE_2020: 0x0f4c81,
+  NIGHT_OWL_BLUE: "#000c1d"
 };
