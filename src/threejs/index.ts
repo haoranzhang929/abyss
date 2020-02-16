56c4e79c3f4838921ca906d202aa393467a394c1
Haoran Zhang <haoranzhang@icloud.com>
2020-02-16 11:58:50 +0000

    update color
    update color

--- src/threejs/index.ts
+++ src/threejs/index.ts
@@ -19,7 +19,9 @@ import {
 
 import fontJson from "../assets/hao.json";
 
-const pantoneColor = 0x0f4c81;
+import { colorPalette } from "./config";
+
+const { WHITE, LIGHT_CREAM, LIGHT_YELLOW, PANTONE_BLUE_2020 } = colorPalette;
 
 export const setupScene = (bgColor: string | number | Color | undefined) => {
   const scene = new Scene();
@@ -35,19 +37,18 @@ export const setupCamera = (windowWidth: number, windowHeight: number) => {
 };
 
 export const setupLights = () => {
-  const directionalLight = new DirectionalLight(0xffccaa, 2);
-  const directionalLight2 = new DirectionalLight(0xffccaa, 0.6);
+  const directionalLight = new DirectionalLight(LIGHT_YELLOW, 2);
+  const directionalLight2 = new DirectionalLight(LIGHT_YELLOW, 0.6);
   directionalLight.position.set(0, 200, -1000);
   directionalLight2.position.set(0, 200, 3000);
-  const ambientLight = new AmbientLight(0x404040, 1);
-  const hemisphereLight = new HemisphereLight(0x404040, 0x020202, 1);
+  const ambientLight = new AmbientLight(LIGHT_CREAM, 0.4);
+  const hemisphereLight = new HemisphereLight(LIGHT_CREAM, PANTONE_BLUE_2020, 0.8);
   return [directionalLight, directionalLight2, ambientLight, hemisphereLight];
 };
 
 export const setupRenderer = (windowWidth: number, windowHeight: number) => {
   const renderer = new WebGLRenderer({ antialias: true });
   renderer.setSize(windowWidth, windowHeight);
-  document.body.appendChild(renderer.domElement);
   return renderer;
 };
 
@@ -55,7 +56,7 @@ export const createFontMesh = (textGeometry: TextBufferGeometry) =>
   new Mesh(
     textGeometry,
     new MeshPhysicalMaterial({
-      color: pantoneColor,
+      color: PANTONE_BLUE_2020,
       metalness: 0.5,
       roughness: 0.5,
       opacity: 0.5,
@@ -67,7 +68,7 @@ export const createFontLine = (textGeometry: TextBufferGeometry) =>
   new LineSegments(
     new EdgesGeometry(textGeometry),
     new LineBasicMaterial({
-      color: 0xffffff,
+      color: WHITE,
       linewidth: 1
     })
   );
@@ -86,7 +87,7 @@ export const loadText = () => {
 
 export const setupCircle = () => {
   const circleGeo = new CircleGeometry(300, 50);
-  const circleMat = new MeshBasicMaterial({ color: 0xffccaa });
+  const circleMat = new MeshBasicMaterial({ color: LIGHT_YELLOW });
   const circle = new Mesh(circleGeo, circleMat);
   circle.position.set(0, 200, -1000);
   return circle;
