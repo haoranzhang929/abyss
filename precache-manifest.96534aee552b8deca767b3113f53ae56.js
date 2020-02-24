dcda2cea94d0ab59c1ec07a0e464ce883ed246a5
Haoran Zhang <haoranzhang@icloud.com>
2020-02-24 23:20:38 +0000

    Updates

--- precache-manifest.96534aee552b8deca767b3113f53ae56.js
+++ precache-manifest.96534aee552b8deca767b3113f53ae56.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "883f443e25c4e0ca3791627d14192c23",
+    "revision": "5c31f17c1169057890de73bda2112d88",
     "url": "/abyss/index.html"
   },
   {
-    "revision": "1e03b7f75c0552e1503e",
+    "revision": "9cf0e5fcf2851915081b",
     "url": "/abyss/static/css/main.15287e76.chunk.css"
   },
   {
-    "revision": "a373cc2999c6fca890d3",
-    "url": "/abyss/static/js/2.4ec0fda8.chunk.js"
+    "revision": "6afa5af02065323fd5bc",
+    "url": "/abyss/static/js/2.56128e12.chunk.js"
   },
   {
     "revision": "07de372803ed01df2e8c300afca1a880",
-    "url": "/abyss/static/js/2.4ec0fda8.chunk.js.LICENSE.txt"
+    "url": "/abyss/static/js/2.56128e12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1e03b7f75c0552e1503e",
-    "url": "/abyss/static/js/main.329d4c87.chunk.js"
+    "revision": "9cf0e5fcf2851915081b",
+    "url": "/abyss/static/js/main.0f457b7d.chunk.js"
   },
   {
     "revision": "17f42b77e2aa3aa9be11",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/abyss/precache-manifest.13e20486baed2fb9e83c2eb8956b6540.js"
+  "/abyss/precache-manifest.96534aee552b8deca767b3113f53ae56.js"
 );
 
 self.addEventListener('message', (event) => {
